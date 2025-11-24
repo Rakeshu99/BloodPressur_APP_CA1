1089b4b77244d882b1642a339724600aed42f944
Rakesh <connectrakeshu99@gmail.com.com>
2025-11-24 16:59:02 +0000

    updated yml and also cleared all the erros post adding bdd test

---
+++ BPCalculator.BDDTests/Features/Feature1.feature
@@ -0,0 +1,28 @@
+﻿Feature: Blood Pressure Classification
+  As a user
+  I want to know my blood pressure category
+  So that I can understand my health status
+
+  Scenario: High blood pressure when systolic is high
+    Given the systolic value is 150
+    And the diastolic value is 70
+    When I calculate the blood pressure category
+    Then the category should be "High"
+
+  Scenario: Pre-high blood pressure
+    Given the systolic value is 130
+    And the diastolic value is 85
+    When I calculate the blood pressure category
+    Then the category should be "PreHigh"
+
+  Scenario: Ideal blood pressure
+    Given the systolic value is 110
+    And the diastolic value is 75
+    When I calculate the blood pressure category
+    Then the category should be "Ideal"
+
+  Scenario: Low blood pressure
+    Given the systolic value is 80
+    And the diastolic value is 55
+    When I calculate the blood pressure category
+    Then the category should be "Low"
---
+++ BPCalculator.BDDTests/StepDefinitions/BloodPressureSteps.cs
@@ -0,0 +1,39 @@
+﻿using BPCalculator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Reqnroll;
+
+namespace BPCalculator.BDDTests.StepDefinitions
+{
+    [Binding]
+    public class BloodPressureSteps
+    {
+        private BloodPressure _bp;
+        private BPCategory _result;
+
+        [Given(@"the systolic value is (.*)")]
+        public void GivenTheSystolicValueIs(int systolic)
+        {
+            _bp ??= new BloodPressure();
+            _bp.Systolic = systolic;
+        }
+
+        [Given(@"the diastolic value is (.*)")]
+        public void GivenTheDiastolicValueIs(int diastolic)
+        {
+            _bp ??= new BloodPressure();
+            _bp.Diastolic = diastolic;
+        }
+
+        [When(@"I calculate the blood pressure category")]
+        public void WhenICalculateTheBloodPressureCategory()
+        {
+            _result = _bp.Category;
+        }
+
+        [Then(@"the category should be ""(.*)""")]
+        public void ThenTheCategoryShouldBe(string expectedCategory)
+        {
+            Assert.AreEqual(expectedCategory, _result.ToString());
+        }
+    }
+}
