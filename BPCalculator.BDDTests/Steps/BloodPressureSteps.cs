1c854a9bd8834ae6d97a01512f527cb92d636f9c
Rakesh <connectrakeshu99@gmail.com.com>
2025-12-09 13:04:49 +0000

    BDD: Added pulse pressure scenario and step definition

--- BPCalculator.BDDTests/Steps/BloodPressureSteps.cs
+++ BPCalculator.BDDTests/Steps/BloodPressureSteps.cs
@@ -63,7 +63,6 @@ namespace BPCalculator.BDDTests.Steps
         {
             Assert.NotNull(_caughtException);
         }
-
         [Then(@"the pulse pressure should be (.*)")]
         public void ThenThePulsePressureShouldBe(int expected)
         {
